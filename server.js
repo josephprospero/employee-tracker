95c1e8a433cbf51bbfe72dd5adfe574a538290d9
Joseph Prospero <jprospero413@gmail.com>
2021-03-06 18:41:55 -0800

    created main menu selector

---
+++ server.js
@@ -0,0 +1,38 @@
+const inquirer = require('inquirer');
+const mysql2 = require('mysql2');
+
+const connection = mysql2.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: '',
+    database: 'employee_db'
+});
+connection.connect(err => {
+    if (err) throw err;
+    console.log("Welcome!")
+    console.log('connected as id ' + connection.threadId + '\n');
+    mainMenu();
+});
+
+function mainMenu() {
+    inquirer.prompt([
+        {
+            type: 'list',
+            message: 'What would you like to do?',
+            name: 'action',
+            choices: [
+                "View all departments",
+                "View all roles",
+                "Add a department",
+                "Add a role",
+                "Add an employee",
+                "Update employee role"
+            ]
+        }
+    ]).then(({ action }) => {
+        console.log(data);
+        }
+    );
+    connection.end()
+}
